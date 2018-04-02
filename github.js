29e8126af6b1dd38b56ef830c50cd39089ca5609
amit <amitmishra4893@gmail.com>
2018-04-02 15:02:06 +0530

    #github.js file has updated

--- github.js
+++ github.js
@@ -6,8 +6,9 @@
         this.repos_short = 'created: asc';
     }
      async getUser(user){
-         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id =${this.client_id}&client_secret=${this.client_secret}`);
-          const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&short=${this.repos_short}&client_id =${this.client_id}&client_secret=${this.client_secret}`);
+         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id        =${this.client_id}&client_secret=${this.client_secret}`);
+         
+          const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&short=${this.repos_short}&client_id         =${this.client_id}&client_secret=${this.client_secret}`);
          const profile = await profileResponse.json();
          const repos = await repoResponse.json();
          return {
